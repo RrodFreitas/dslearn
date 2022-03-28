ef06a7156c7c18c99b89d6e6e684067ab08699d8
Rodrigo Von-Grapp Freitas <rodrigo.orochi@gmail.com>
2022-03-28 15:29:18 -0300

    Course

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Offer.java
@@ -0,0 +1,81 @@
+package com.devsuperior.dslearnbds.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_offer")
+public class Offer implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
+	private String edition;
+	private Instant startMoment;
+	private Instant endMoment;
+	
+	public Offer() {
+	}
+	
+	public Offer(Long id, String edition, Instant startMoment, Instant endMoment) {
+		this.id = id;
+		this.edition = edition;
+		this.startMoment = startMoment;
+		this.endMoment = endMoment;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEdition() {
+		return edition;
+	}
+
+	public void setEdition(String edition) {
+		this.edition = edition;
+	}
+
+	public Instant getStartMoment() {
+		return startMoment;
+	}
+
+	public void setStartMoment(Instant startMoment) {
+		this.startMoment = startMoment;
+	}
+
+	public Instant getEndMoment() {
+		return endMoment;
+	}
+
+	public void setEndMoment(Instant endMoment) {
+		this.endMoment = endMoment;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Offer other = (Offer) obj;
+		return Objects.equals(id, other.id);
+	}
+}
